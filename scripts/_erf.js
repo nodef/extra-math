eb1d8d209bcecf1dc25826a4ac6be4cd4c706844
wolfram77 <wolfram77@gmail.com>
2020-02-28 02:35:39 +0530

    erf: add somtime later

--- scripts/_erf.js
+++ scripts/_erf.js
@@ -5,13 +5,17 @@ const A4 = -1.453152027;
 const A5 =  1.061405429;
 const P  =  0.3275911;
 
-function erf(x) {
-  var sgn = x<0? -1:1;
-  var x = Math.abs(x);
-  var t = 1/(1+ P*x);
-  var y = 1-(((((A5*t+A4)*t)+A3)*t+A2)*t+A1)*t*Math.exp(-x*x);
+/**
+ * Gives error function value of number (approximation).
+ * @param {number} n a number
+ */
+function erf(n) {
+  var sgn = n<0? -1:1;
+  var n = Math.abs(n);
+  var t = 1/(1+ P*n);
+  var y = 1-(((((A5*t+A4)*t)+A3)*t+A2)*t+A1)*t*Math.exp(-n*n);
   return sgn*y;
-};
+}
 module.exports = erf;
 // https://github.com/AndreasMadsen/mathfn/blob/master/functions/erf.js
 // http://www.johndcook.com/cpp_erf.html
