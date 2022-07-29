ba36df5f65f78932260017f579e5cea69e9c215c
Subhajit Sahu <wolfram77@gmail.com>
2022-07-29 21:46:18 +0530

    :mango: update build setup

---
+++ tests/index.test.ts
@@ -0,0 +1,23 @@
+import * as math from "../src";
+
+
+
+
+// 1. Basic tests.
+test("example1", () => {
+  var a = math.sum(1, 2, 3, 4);
+  expect(a).toBe(10);
+  // → 10
+
+  var a = math.median(1, 7, 8);
+  expect(a).toBe(7);
+  // → 7
+
+  var a = math.variance(1, 2, 3, 4);
+  expect(a).toBe(1.25);
+  // → 1.25
+
+  var a = math.lcm(2, 3, 4);
+  expect(a).toBe(12);
+  // → 12
+)};
