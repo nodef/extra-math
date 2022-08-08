86784a54950aa3fcd598bc27fe196edf6aa4ca11
Subhajit Sahu <wolfram77@gmail.com>
2022-08-08 12:24:00 +0530

    :mango: re-export from extra-number

--- src/index.ts
+++ src/index.ts
@@ -1,345 +1,90 @@
-// NATURAL NUMBERS
-// ===============
+// METHODS
+// =======
 
-/**
- * Find the remainder of x/y with sign of x (truncated division).
- * @param x dividend
- * @param y divisor
- * @returns trunc(x/y)
- */
-export function rem(x: number, y: number): number {
-  return x % y;
-}
-// - https://en.wikipedia.org/wiki/Modulo_operation
+// ROUND
+// -----
 
-
-/**
- * Find the remainder of x/y with sign of y (floored division).
- * @param x dividend
- * @param y divisor
- * @returns floor(x/y)
- */
-export function mod(x: number, y: number): number {
-  return x - y*Math.floor(x/y);
-}
-// - https://en.wikipedia.org/wiki/Modulo_operation
-
-
-/**
- * Find the remainder of x/y with +ve sign (euclidean division).
- * @param x dividend
- * @param y divisor
- * @returns n>0: floor(x/y), n<0: ceil(x/y)
- */
-export function modp(x: number, y: number): number {
-  return x - Math.abs(y)*Math.floor(x/Math.abs(y));
-}
-// - https://en.wikipedia.org/wiki/Modulo_operation
-
-
-// Find the greatest common divisor of a pair of numbers.
-function gcdPair(x: number, y: number): number {
-  while (y!==0) {
-    var t = y;
-    y = x % y;
-    x = t;
-  }
-  return x;
-}
-// - https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
-// - https://en.wikipedia.org/wiki/Euclidean_algorithm
-
-/**
- * Find the greatest common divisor of numbers.
- * @param xs a list of numbers
- * @returns gcd(x₁, x₂, ...)
- */
-export function gcd(...xs: number[]): number {
-  var a = xs[0] || 1;
-  for(var i=1, I=xs.length; i<I; i++)
-    a = gcdPair(a, xs[i]);
-  return a;
-}
-export {gcd as hcf};
-
-
-/**
- * Find the least common multiple of numbers.
- * @param xs a list of numbers
- * @returns lcm(x₁, x₂, ...)
- */
-export function lcm(...xs: number[]): number {
-  var a = xs[0] || 1;
-  for (var i=1, I=xs.length; i<I; i++)
-      a = a*xs[i] / gcdPair(a, xs[i]);
-  return a;
-}
-// - https://en.wikipedia.org/wiki/Least_common_multiple
-
-
-/**
- * Find the factorial of a number.
- * @param n a number
- * @param k denominator factorial [0]
- * @returns P(n, k); k=0: n!, k>0: n!/k!
- */
-export function factorial(n: number, k: number=0): number {
-  if (n<0) return 0;
-  for (var i=k+1, a=1; i<=n; i++)
-    a *= i;
-  return a;
-}
-// - https://github.com/alawatthe/MathLib/blob/master/src/Functn/functions/factorial.ts
-// - https://en.wikipedia.org/wiki/Permutation
-
-
-/**
- * Find the number of ways to choose k elements from a set of n elements.
- * @param n elements in source set
- * @param k elements in choose set
- * @returns C(n, k)
- */
-export function binomial(n: number, k: number): number {
-  // Generalization to negative integers
-  if (k<0 || k>Math.abs(n)) return 0;
-  if (n<0) return Math.pow(-1, k)*binomial(-n, k);
-  // Take advantage of symmetry
-  k = k>n-k? n-k:k;
-  for (var a=1, i=1; i<=k; i++, n--)
-    a *= n/i;
-  return a;
-}
-// - https://github.com/alawatthe/MathLib/blob/master/src/Functn/functions/binomial.ts
-// - https://en.wikipedia.org/wiki/Binomial_coefficient
-
-
-/**
- * Find the number of ways to put n objects in m bins (n=sum(kᵢ)).
- * @param ks objects per bin (kᵢ)
- * @returns n!/(k₁!k₂!...) | n=sum(kᵢ)
- */
-export function multinomial(...ks: number[]): number {
-  var n = sum(...ks), a = 1;
-  for (var i=0, j=0, I=ks.length; i<I;) {
-    if (j<=0) j = ks[i++];
-    else a *= n--/j--;
-  }
-  return a;
-}
-// - https://en.wikipedia.org/wiki/Multinomial_distribution
+export {floor} from "extra-number";
+export {ceil}  from "extra-number";
+export {round} from "extra-number";
 
 
 
 
-// REAL NUMBERS
-// ============
+// ROUNDED DIVISION
+// ----------------
 
-/**
- * Constrain a number within a minimum and a maximum value.
- * @param x a number
- * @param min minimum value
- * @param max maximum value
- * @returns x<min: min, x>max: max, x
- */
-export function constrain(x: number, min: number, max: number): number {
-  return Math.min(Math.max(x, min), max);
-}
-export {constrain as clamp};
-// - https://processing.org/reference/constrain_.html
-// - https://www.npmjs.com/package/clamp
-// - https://en.cppreference.com/w/cpp/algorithm/clamp
-// - https://dlang.org/library/std/algorithm/comparison/clamp.html
-// - https://www.rdocumentation.org/packages/raster/versions/3.0-12/topics/clamp
-// - https://docs.microsoft.com/en-us/dotnet/api/system.math.clamp?view=netcore-3.1
-// - https://docs.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-clamp
-// - https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/clamp.xhtml
-// - https://docs.unity3d.com/ScriptReference/Mathf.Clamp.html
-// - https://en.wikipedia.org/wiki/Clamping_(graphics)
+export {floorDiv} from "extra-number";
+export {ceilDiv}  from "extra-number";
+export {roundDiv} from "extra-number";
 
 
-/**
- * Normalize a number from its current range into a value between 0 and 1.
- * @param x a number
- * @param r lower bound of current range
- * @param R upper bound of current range
- * @returns ∈ [0, 1]
- */
-export function normalize(x: number, r: number, R: number): number {
-  return (x - r)/(R - r);
-}
-export {normalize as norm};
-// - https://processing.org/reference/norm_.html
 
 
-/**
- * Re-map a number from one range to another.
- * @param x a number
- * @param r lower bound of current range
- * @param R upper bound of current range
- * @param t lower bound of target range
- * @param T upper bound of target range
- * @returns ∈ [ymin, ymax]
- */
-export function remap(x: number, r: number, R: number, t: number, T: number): number {
-  return t + ((x - r)/(R - r)) * (T - t);
-}
-export {remap as map};
-// - https://processing.org/reference/map_.html
+// MODULO
+// ------
+
+export {rem}  from "extra-number";
+export {mod}  from "extra-number";
+export {modp} from "extra-number";
 
 
-/**
- * Linearly interpolate a number between two numbers.
- * @param x start number
- * @param y stop number
- * @param t interpolant ∈ [0, 1]
- * @returns ∈ [x, y]
- */
-export function lerp(x: number, y: number, t: number): number {
-  return x + t*(y - x);
-}
-// - https://processing.org/reference/lerp_.html
-// - https://docs.unity3d.com/ScriptReference/Vector3.Lerp.html
 
 
-/**
- * Find the nth root of a number (ⁿ√).
- * @param x a number
- * @param n root
- * @returns ⁿ√x
- */
-export function root(x: number, n: number): number {
-  if ((n & 1)===0) return Math.pow(x, 1/n);
-  return   Math.sign(x) * Math.pow(Math.abs(x), 1/n);
-}
-// - https://github.com/alawatthe/MathLib/blob/master/src/Functn/functions/root.ts
+// RANGE CONTROL
+// -------------
+
+export {constrain} from "extra-number";
+export {normalize} from "extra-number";
+export {remap}     from "extra-number";
+export {lerp}      from "extra-number";
+export {constrain as clamp} from "extra-number";
+export {normalize as norm}  from "extra-number";
+export {remap as map}       from "extra-number";
 
 
-/**
- * Find the logarithm of a number with a given base.
- * @param x a number
- * @param b logarithm base [e]
- * @returns log_b (x)
- */
-export function log(x: number, b: number=Math.E): number {
-  return Math.log(x)/Math.log(b);
-}
-// - https://en.wikipedia.org/wiki/Logarithm
 
 
-// function powAbs(x: number, n: number): number {
-//   return Math.sign(x) * Math.pow(Math.abs(x), n);
-// }
+// ARITHMETIC
+// ----------
 
-/**
- * Compute the gamma function of a number (Γ).
- * @param x a number
- * @returns Γ(x); for +ve integer Γ(x) = x!
- */
-// function gamma(x: number): number {
-//   var x  = x - 1;
-//   var e1 = Math.sqrt(Math.abs(2*Math.PI*x));
-//   var e2 = powAbs(x/Math.E, x);
-//   var e3 = powAbs(x*Math.sinh(1/x), x/2);
-//   var e4 = Math.exp((7/324)*(1/((x**3) * (35*x**2 + 33))));
-//   return e1*e2*e3*e4;
-// }
-// - https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5840229/
+export {isPow}   from "extra-number";
+export {prevPow} from "extra-number";
+export {nextPow} from "extra-number";
+export {root}    from "extra-number";
+export {log}     from "extra-number";
 
 
-/**
- * Compute the natural logarithm of the absolute value of the gamma function of a number (log-Γ).
- * @param x a number
- * @returns log(|Γ(x)|)
- */
-// function lgamma(x: number): number {
-//   return Math.log(Math.abs(gamma(x)));
-// }
-// - https://en.cppreference.com/w/cpp/numeric/math/lgamma
 
 
-const ERF_A1 =  0.254829592;
-const ERF_A2 = -0.284496736;
-const ERF_A3 =  1.421413741;
-const ERF_A4 = -1.453152027;
-const ERF_A5 =  1.061405429;
-const ERF_P  =  0.3275911;
+// DIVISORS
+// --------
 
-/**
- * Find error function value of number (approximation).
- * @param x a number
- * @returns erf(x)
- */
-export function erf(x: number): number {
-  var sgn = x<0? -1:1;
-  var x = Math.abs(x);
-  var t = 1/(1+ ERF_P*x);
-  var y = 1-(((((ERF_A5*t+ERF_A4)*t)+ERF_A3)*t+ERF_A2)*t+ERF_A1)*t*Math.exp(-x*x);
-  return sgn*y;
-}
-// - https://github.com/AndreasMadsen/mathfn/blob/master/functions/erf.js
-// - http://www.johndcook.com/cpp_erf.html
-// - https://en.wikipedia.org/wiki/Error_function
+export {properDivisors} from "extra-number";
+export {aliquotSum}     from "extra-number";
+export {isPrime}        from "extra-number";
+export {gcd}            from "extra-number";
+export {lcm}            from "extra-number";
+export {gcd as hcf}     from "extra-number";
 
 
-const ERFC_TABLE = [
-  -2.8e-17, 1.21e-16, -9.4e-17, -1.523e-15, 7.106e-15,
-   3.81e-16, -1.12708e-13, 3.13092e-13, 8.94487e-13,
-  -6.886027e-12, 2.394038e-12, 9.6467911e-11,
-  -2.27365122e-10, -9.91364156e-10, 5.059343495e-9,
-   6.529054439e-9, -8.5238095915e-8, 1.5626441722e-8,
-   1.303655835580e-6, -1.624290004647e-6,
-  -2.0278578112534e-5, 4.2523324806907e-5,
-   3.66839497852761e-4, -9.46595344482036e-4,
-  -9.561514786808631e-3, 1.9476473204185836e-2,
-   6.4196979235649026e-1, -1.3026537197817094
-];
 
-/**
- * Find the complementary error function value of number (approximation).
- * @param x a number
- * @returns erfc(x)
- */
-export function erfc(x: number): number {
-  if(x<0) return 2-erfc(-x);
-  var c = 0, d = 0, y = 2/(2+x), z = 4*y-2;
-  for(var i=0, I=ERFC_TABLE.length-1; i<I; i++) {
-    var t = d;
-    d = z*d-c+ERFC_TABLE[i];
-    c = t;
-  }
-  return y*Math.exp(-x*x+0.5*(ERFC_TABLE[I]+z*d)-c);
-}
-// - https://github.com/AndreasMadsen/mathfn/blob/master/functions/erf.js
-// - http://pydoc.net/Python/timbre/1.0.0/timbre.stats/
-// - https://github.com/jstat/jstat/blob/master/src/special.js
-// - https://en.wikipedia.org/wiki/Error_function
+
+// ARRANGEMENTS
+// ------------
+
+export {factorial}   from "extra-number";
+export {binomial}    from "extra-number";
+export {multinomial} from "extra-number";
 
 
 
 
 // GEOMETRY
-// ========
+// --------
 
-/**
- * Convert radians to degrees.
- * @param x radians
- * @returns 2π → 360
- */
-export function degrees(x: number): number {
-  return x*(180/Math.PI);
-}
-// - https://processing.org/reference/degrees_.html
-
-
-/**
- * Convert degrees to radians.
- * @param x degrees
- * @returns 360 → 2π
- */
-export function radians(x: number): number {
-  return x*(Math.PI/180);
-}
-// - https://processing.org/reference/radians_.html
+export {degrees} from "extra-number";
+export {radians} from "extra-number";
 
 
 /**
@@ -374,113 +119,23 @@ export function distance(xs: number[], ys: number[]): number {
 
 
 // STATISTICS
-// ==========
+// ----------
 
-/**
- * Find the sum of numbers (Σ).
- * @param xs a list of numbers
- * @returns Σxᵢ
- */
-export function sum(...xs: number[]): number {
-  var a = 0;
-  for (var x of xs)
-    a += x;
-  return a;
-}
+export {sum}      from "extra-number";
+export {product}  from "extra-number";
+export {median}   from "extra-number";
+export {modes}    from "extra-number";
+export {range}    from "extra-number";
+export {variance} from "extra-number";
 
 
-/**
- * Find the product of numbers (∏).
- * @param xs a list of numbers
- * @returns ∏xᵢ
- */
-export function product(...xs: number[]): number {
-  var a = 1;
-  for (var x of xs)
-    a *= x;
-  return a;
-}
 
 
-/**
- * Find the average of numbers.
- * @param xs a list of numbers
- * @returns Σxᵢ/n | n = size(xs)
- */
-export function mean(...xs: number[]): number {
-  if (xs.length===0) return 0;
-  return sum(...xs)/xs.length;
-}
+// MEAN (STATISTICS)
+// -----------------
 
-
-/**
- * Find the value separating the higher and lower halves of numbers.
- * @param xs a list of numbers
- * @returns xₘ | sort(xs) = [..., xₘ, ...]
- */
-export function median(...xs: number[]): number {
-  if (xs.length===0) return 0;
-  xs.sort((a, b) => a-b);
-  var i = xs.length>>1;
-  if ((xs.length & 1)===1) return xs[i];
-  return (xs[i-1] + xs[i])/2;
-}
-// - https://stackoverflow.com/questions/45309447/calculating-median-javascript
-// - https://en.wikipedia.org/wiki/Median
-
-
-// Get the maximum number of times any number has repeated in a sorted array.
-function maxRepeat(xs: number[]): number {
-  var count = Math.min(xs.length, 1), max = count;
-  for (var i=1, I=xs.length; i<I; i++) {
-    if (xs[i-1]===xs[i]) count++;
-    else { max = Math.max(max, count); count = 1; }
-  }
-  return Math.max(max, count);
-}
-
-// Get the numbers which have been repeated atleast given number of times.
-function getRepeats(xs: number[], r: number): number[] {
-  var a: number[] = []; r--;
-  for (var i=0, I=xs.length-r; i<I; i++)
-    if (xs[i]===xs[i+r]) a.push(xs[i+=r]);
-  return a;
-}
-
-/**
- * Find the values that appear most often.
- * @param xs a list of numbers
- * @returns [xₘ₁, xₘ₂, ...] | count(xₘᵢ) ≥ count(xᵢ) ∀ xᵢ ∈ xs
- */
-export function modes(...xs: number[]): number[] {
-  xs.sort((a, b) => a-b);
-  var r = maxRepeat(xs);
-  return getRepeats(xs, r);
-}
-// - https://en.wikipedia.org/wiki/Mode_(statistics)
-
-
-/**
- * Find the difference between the largest and smallest values.
- * @param xs a list of numbers
- * @returns max(xs) - min(xs)
- */
-export function range(...xs: number[]): number {
-  return Math.max(...xs) - Math.min(...xs);
-}
-// - https://en.wikipedia.org/wiki/Range_(statistics)
-
-
-/**
- * Find the mean of squared deviation of numbers from its mean.
- * @param xs a list of numbers
- * @returns σ² = E[(xs - µ)²] | µ = mean(xs)
- */
-export function variance(...xs: number[]): number {
-  if (xs.length===0) return 0;
-  var m = mean(...xs), a = 0;
-  for (var x of xs)
-    a += (x-m)**2;
-  return a/xs.length;
-}
-// - https://en.wikipedia.org/wiki/Variance
+export {arithmeticMean} from "extra-number";
+export {geometricMean}  from "extra-number";
+export {harmonicMean}   from "extra-number";
+export {quadriaticMean} from "extra-number";
+export {cubicMean}      from "extra-number";
